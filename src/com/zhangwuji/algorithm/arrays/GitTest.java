d0f74a69f157dd0bfac3e0caafed7277924631ed
zhangwuji <zhangwuji199807@gamil.com>
2021-08-20 07:24:43 +0800

    张无忌4次提交

--- src/com/zhangwuji/algorithm/arrays/GitTest.java
+++ src/com/zhangwuji/algorithm/arrays/GitTest.java
@@ -5,4 +5,7 @@ package com.zhangwuji.algorithm.arrays;
  * @date 2021/8/20  --7:02
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hot-fix");
+    }
 }
